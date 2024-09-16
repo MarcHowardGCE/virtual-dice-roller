ef9d93190936a19c7f66ceaadcf9b9714d433b23
Marc Howard <marc.howard@gce.com>
2024-09-16 12:17:49 -0700

    update ws to wss

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket(`ws://${window.location.host}`);
+const ws = new WebSocket(`wss://${window.location.host}`);
 
 let nickname = '';
 const diceButtons = document.querySelectorAll('.dice-button');
